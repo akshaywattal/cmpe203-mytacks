48ad719359524b92cce34e13f9cd60f2cd28478e
oldmonk7 <shashankgarg7@gmail.com>
2014-05-04 15:54:21 -0700

    added dropdown for boards

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,21 +1,24 @@
 'use strict';
 
 angular.module('mytacksApp')
-.controller('HomeCtrl', function($scope, User, $http, Session, $location){
+.controller('HomeCtrl', function($scope, User, $http, Session, $location, $rootScope){
 
+	$scope.user = $rootScope.currentUser;
 	//$scope.user = currentUser.name || '';
 
 	// $http.get('/api/users/me').success(function(user) {
  //      $scope.user = user.name;
  //      console.dir(user);
  //    });
+	
+	$scope.boards = ['myboard', 'yourboard', 'imbored'];	
 	$scope.formData = {};
 	$scope.addTack = function(){
 		
 		$http.post('/api/addtack', $scope.formData)
 		.success(function(){ 
 
-			alert("Added Tack");
+			alert('Added Tack');
 			//$scope.posts = $http.get('/api/posts').success(function(data){$scope.posts = data;});
 		});
 		// $scope.posts.push($scope.post);
